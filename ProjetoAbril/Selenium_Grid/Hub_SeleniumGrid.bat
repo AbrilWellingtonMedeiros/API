402c17f685271f7d1715fa31651da5fb3a3e4348
wellington.medeiros <wellington.medeiros@abril.com.br>
2022-06-30 18:56:04 -0300

    projeto apps

--- ProjetoAbril/Selenium_Grid/Hub_SeleniumGrid.bat
+++
@@ -1,1 +0,0 @@
-CALL java -jar C:\Users\a515360\git\automated-tests\ProjetoExemplo\utilitarios\libs\selenium-server-standalone-3.4.0.jar -role hub -host 192.168.0.8
--- ProjetoAbril/ftor_bat.bat
+++
@@ -1,8 +0,0 @@
-cd C:\Users\a515360\git\automated-tests\ProjetoExemplo
-set ProjectPath=C:\Users\a515360\git\automated-tests\ProjetoExemplo
-set ftor=C:\Users\a515360\git\automated-tests\ProjetoExemplo\bin\grid_paralelo\
-echo %ProjectPath%
-set classpath=%ProjectPath%\bin;%ProjectPath%\libs\*
-echo %classpath%
-java org.testng.TestNG -log 100 %ftor%\testng_paralelo.xml
-pause
